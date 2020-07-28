a1838e85409639729ad0954115a9b98a04923fd7
Dallin <dallinis@hotmail.com>
2020-07-27 19:35:35 -0600

    add postgres and migrations

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+source ./.env
+docker run -v $(pwd)/migrations:/migrations --network host migrate/migrate -path=/migrations/ -database "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_DATABASE}?sslmode=disable" up
---
+++ migrations/1_create_daily_prices_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE daily_prices (
+    symbol VARCHAR(15) NOT NULL,
+    date DATE NOT NULL,
+    open_price NUMERIC(20, 8) NOT NULL,
+    high_price NUMERIC(20, 8) NOT NULL,
+    low_price NUMERIC(20, 8) NOT NULL,
+    close_price NUMERIC(20, 8) NOT NULL,
+    volume integer
+)
